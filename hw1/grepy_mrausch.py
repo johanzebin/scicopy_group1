fe413fd4589c318239468ed69b6b4f88bb922bd8
Michael Rausch <mrausch@uni-osnabrueck.de>
2011-05-18 15:10:33 +0200

    Corrected some comment formatting, added time tracking (-t)

--- hw1/grepy_mrausch.py
+++ hw1/grepy_mrausch.py
@@ -6,7 +6,7 @@
 # University of Osnabrueck
 # Homework 1 for the SciCoPy Course SS2011
  
-import re, os
+import re, os, time
 from optparse import OptionParser
 
 def main():
@@ -19,7 +19,7 @@ def main():
     # -f --filenames: print filenames before occurences
     # -R --recursive: recursively process directories
     # -v: Print author details and version number
-        
+    start_time = time.clock()    
     # set usage info string and version string and create optparser
     usage = "%prog PATTERN FILE [FILE] [Options]"
     version = "%prog 0.1-mr"
@@ -32,24 +32,39 @@ def main():
     # by default, optparse puts this at the top of the option list, which is ugly
     parser.set_conflict_handler(handler="resolve")
 
-    parser.add_option("-c", "--color", action="store_true", dest="color", help="Highlight pattern matches with color")
-    parser.add_option("-h", "-?", "--help", action="help", help="Display this help page") # define the custom help switch
-    parser.add_option("-n", "--line-numbers", action="store_true", dest="println", help="Print line-numbers of matches")
-    parser.add_option("-f", "--filenames", action="store_true", dest="printfn", help="Print file names before matches")
-    parser.add_option("-R", "--recursive", action="store_true", dest="recursive", help="Recursively process directories in the file list")
-    parser.add_option("-v", "--version", action="version", help="Show author and version information and exit.")
+    parser.add_option("-c", "--color", action="store_true", dest="color",\
+                       help="Highlight pattern matches with color")
+    # define the custom help switch
+    parser.add_option("-h", "-?", "--help", action="help", help="Display this help page")
+    parser.add_option("-n", "--line-numbers", action="store_true", dest="println",\
+                       help="Print line-numbers of matches")
+    parser.add_option("-f", "--filenames", action="store_true", dest="printfn",\
+                       help="Print file names before matches")
+    parser.add_option("-R", "--recursive", action="store_true", dest="recursive",\
+                       help="Recursively process directories in the file list")
+    parser.add_option("-v", "--version", action="version",\
+                       help="Show author and version information and exit.")
+    parser.add_option("-t", "--time", action="store_true", dest="time",\
+                       help="Print out the processing time at the end of script run")
 
-    (options, args) = parser.parse_args() # get options and arguments from argv 
-
-    if len(args) < 2: # print usage information if no or too less arguments are supplied
-        parser.print_help() # print usage info
+    # get options and arguments from argv
+    (options, args) = parser.parse_args()
+     
+    # print usage information if no or too less arguments are supplied
+    if len(args) < 2:
+        parser.print_help()
         exit(0) # and quit
 
     pattern = args[0] # first argument is the regex pattern
-    reg = re.compile(pattern)
+    reg = re.compile(pattern) # precompile the pattern (more efficient)
     files = args[1:] # all other arguments are filenames
     
+    # Everything's set up, so start working
     grepy(files,reg,options)
+    
+    if options.time:
+        end_time = time.clock()
+        print "Script run took "+str(end_time - start_time)+ "s."
     
 def grepy(files,reg,options):
     # iterate over the filenames
@@ -58,26 +73,36 @@ def grepy(files,reg,options):
             if options.recursive:
                 # generate a list of subfiles, joining the current directory path and its sub-entries
                 subfiles = [os.path.join(filename,entry) for entry in os.listdir(filename)]
-                grepy(subfiles,reg,options) # recurse over the files of the directory
-            continue # ...skip the directory, either because we are done recursing, or we are not working recursive at all
-       
-        with open(filename) as infile: # file handles implement context manager for exceptions and stuff, see session 2 slide 21            
-            lines = infile.readlines() # get all the lines. Yes, ALL the lines.
-            for linenumber,line in enumerate(lines): # we want the line numbers too, see session 3 slide 29
-                match = reg.search(line) # check if the line matches the pattern
+                # recurse over the files of the directory
+                grepy(subfiles,reg,options) 
+            # ...skip the directory, either because we are done recursing,
+            # or we are not working recursive at all
+            continue
+        # file handles implement context manager for exceptions and stuff
+        # see session 2 slide 21 (on with statements)
+        with open(filename) as infile:
+            # get all the lines. Yes, ALL the lines. Might be more convenient
+            # to read lines one by one, but the former way allows us to enumerate
+            # lines in advance                         
+            lines = infile.readlines()
+            # we want the line numbers too, see session 3 slide 29
+            for linenumber,line in enumerate(lines):
+                # check if the line matches the pattern 
+                match = reg.search(line)
                 if match:
                     prepend = ""
                     if options.printfn:
                         prepend = filename + ":"
                     if options.println:
-                        prepend = prepend + str(linenumber+1) + ":"                        
-                    # this is ugly, but currently I don't have an idea how to do it different
-                    if options.color: # wrap some magic line chars for green color around the matched pattern
-                        print prepend + re.sub(match.group(0), "\033[92m"+match.group(0)+"\033[0m", line.strip())
-                        # see also: http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+                        prepend = prepend + str(linenumber+1) + ":"                    
+                    if options.color:
+                        # wrap some magic line chars for green color around the matched pattern see also:
+                        # http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+                        print prepend + re.sub(match.group(0), "\033[92m"+match.group(0)+"\033[0m", line.rstrip())                        
                     else:
-                        print prepend + line.strip() # simply print                                         
-                # done with the line
-            # done with the the file
+                        # always strip away line breaks at the end of lines
+                        print prepend + line.rstrip()
+            # done with the line
+        # done with the the file
                     
 if __name__ == '__main__': main()
