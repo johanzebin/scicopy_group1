c1ace1f549e167a016e96c7067a327ac336fcd2f
Michael Rausch <mrausch@uni-osnabrueck.de>
2011-05-18 15:28:43 +0200

    - Added exception handling for IOErrors (happens if a file is not available) - Also added a check for binary files. The script will now skip binary files.

--- hw1/grepy_mrausch.py
+++ hw1/grepy_mrausch.py
@@ -65,44 +65,74 @@ def main():
     if options.time:
         end_time = time.clock()
         print "Script run took "+str(end_time - start_time)+ "s."
+
+# Taken from:
+# http://stackoverflow.com/questions/898669/how-can-i-detect-if-a-file-is-binary-non-text-in-python/3002505#3002505
+def is_binary(filename):
+    """Return true if the given filename is binary.
+    @raise EnvironmentError: if the file does not exist or cannot be accessed.
+    @attention: found @ http://bytes.com/topic/python/answers/21222-determine-file-type-binary-text on 6/08/2010
+    @author: Trent Mick <TrentM@ActiveState.com>
+    @author: Jorge Orpinel <jorge@orpinel.com>"""
+    fin = open(filename, 'rb')
+    try:
+        CHUNKSIZE = 1024
+        while 1:
+            chunk = fin.read(CHUNKSIZE)
+            if '\0' in chunk: # found null byte
+                return True
+            if len(chunk) < CHUNKSIZE:
+                break # done
+    # A-wooo! Mira, python no necesita el "except:". Achis... Que listo es.
+    finally:
+        fin.close()
+
+    return False        
     
 def grepy(files,reg,options):
     # iterate over the filenames
     for filename in files:
-        if os.path.isdir(filename):
-            if options.recursive:
-                # generate a list of subfiles, joining the current directory path and its sub-entries
-                subfiles = [os.path.join(filename,entry) for entry in os.listdir(filename)]
-                # recurse over the files of the directory
-                grepy(subfiles,reg,options) 
-            # ...skip the directory, either because we are done recursing,
-            # or we are not working recursive at all
-            continue
-        # file handles implement context manager for exceptions and stuff
-        # see session 2 slide 21 (on with statements)
-        with open(filename) as infile:
-            # get all the lines. Yes, ALL the lines. Might be more convenient
-            # to read lines one by one, but the former way allows us to enumerate
-            # lines in advance                         
-            lines = infile.readlines()
-            # we want the line numbers too, see session 3 slide 29
-            for linenumber,line in enumerate(lines):
-                # check if the line matches the pattern 
-                match = reg.search(line)
-                if match:
-                    prepend = ""
-                    if options.printfn:
-                        prepend = filename + ":"
-                    if options.println:
-                        prepend = prepend + str(linenumber+1) + ":"                    
-                    if options.color:
-                        # wrap some magic line chars for green color around the matched pattern see also:
-                        # http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
-                        print prepend + re.sub(match.group(0), "\033[92m"+match.group(0)+"\033[0m", line.rstrip())                        
-                    else:
-                        # always strip away line breaks at the end of lines
-                        print prepend + line.rstrip()
-            # done with the line
-        # done with the the file
+        try:
+            if is_binary(filename):
+                print filename+" is a binary file. Skipping file."
+            if os.path.isdir(filename):
+                if options.recursive:
+                    # generate a list of subfiles, joining the current directory path and its sub-entries
+                    subfiles = [os.path.join(filename,entry) for entry in os.listdir(filename)]
+                    # recurse over the files of the directory
+                    grepy(subfiles,reg,options) 
+                # ...skip the directory, either because we are done recursing,
+                # or we are not working recursive at all
+                continue
+            # file handles implement context manager for exceptions and stuff
+            # see session 2 slide 21 (on with statements)
+            with open(filename) as infile:
+                # get all the lines. Yes, ALL the lines. Might be more convenient
+                # to read lines one by one, but the former way allows us to enumerate
+                # lines in advance                         
+                lines = infile.readlines()
+                # we want the line numbers too, see session 3 slide 29
+                for linenumber,line in enumerate(lines):
+                    # check if the line matches the pattern 
+                    match = reg.search(line)
+                    if match:
+                        prepend = ""
+                        if options.printfn:
+                            prepend = filename + ":"
+                        if options.println:
+                            prepend = prepend + str(linenumber+1) + ":"                    
+                        if options.color:
+                            # wrap some magic line chars for green color around the matched pattern see also:
+                            # http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+                            print prepend + re.sub(match.group(0), "\033[92m"+match.group(0)+"\033[0m", line.rstrip())                        
+                        else:
+                            # always strip away line breaks at the end of lines
+                            print prepend + line.rstrip()
+                # done with the line
+            # done with the the file
+        # If something goes wrong with a file, simply skip it
+        # and print an error
+        except IOError:
+            print filename+": Error opening file. Skipping file."
                     
 if __name__ == '__main__': main()
