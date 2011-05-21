47c5555c059cd9e23d10414f8085b10e54bc44f3
Johannes K <johanneskeyser@gmx.de>
2011-05-21 21:44:16 +0200

    implemented core functionality

--- hw1/grepy_jkeyser.py
+++ hw1/grepy_jkeyser.py
@@ -1,9 +1,18 @@
-"""
-asdsadasd
-"""
-from optparse import OptionParser
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
 
-def color_string(text="", color_str="yellow"):
+"""
+grepy implements some core functionality of the UNIX grep command.
+@author: Johannes Keyser (jkeyser@uos.de)
+"""
+
+from optparse import OptionParser
+import os
+import sys
+import re
+
+def _color_string(text="", color_str="yellow"):
     """
     Adds ANSI escape codes to the string 'text' according to a few color
     names handed over as strings in 'color_str'. When printed, the modified
@@ -18,23 +27,62 @@ def color_string(text="", color_str="yellow"):
         raise ValueError(err_str)
     return "\033[0;%dm%s\033[0m" % (_colormap.get(color_str), text)
 
-
-if __name__ == "__main__":
-    PARSER = OptionParser()
-    PARSER.add_option("-c", "--color", dest="colorize_output",
+def _setup_grepy_parser():
+    """
+    Sets up an optparse-command line parser specific for grepy.
+    """
+    usage = "%prog [OPTIONS] PATTERN FILE [FILE...]"
+    versn = "%prog version 1.0 jk"
+    descr = "%prog searches the named input FILEs for lines containing"\
+           +" a match to the given PATTERN and prints them. Links are ignored!"
+    parser = OptionParser(usage=usage, version=versn, description=descr)
+    parser.add_option("-c", "--color", dest="colorize_output",
                       default=False, action="store_true",
                       help="Highlight the matching string in color.")
-    PARSER.add_option("-n", "--line-number", dest="print_line_number",
+    parser.add_option("-n", "--line-number", dest="print_line_number",
                       default=False, action="store_true",
                       help="Prefix output line with the line number.")
-    PARSER.add_option("-f", "--filenames", dest="print_filenames",
+    parser.add_option("-f", "--filenames", dest="print_filenames",
                       default=False, action="store_true",
                       help="Print filenames before occurence of PATTERN.")
-    PARSER.add_option("-R", "--recursive", dest="traverse_recursively",
+    parser.add_option("-R", "--recursive", dest="traverse_recursively",
                       default=False, action="store_true",
                       help="Read all files under each directory, recursively.")
-    PARSER.add_option("--version", dest="show_version_number",
-                      default=False, action="store_true",
-                      help="Prints author details and version number.")
+    return parser
 
-    (OPTIONS, ARGS) = PARSER.parse_args()
+if __name__ == "__main__":
+    GREPY_PARSER    = _setup_grepy_parser()
+    (OPTIONS, ARGS) = GREPY_PARSER.parse_args()
+    # reject if either PATTERN or FILE is missing
+    if len(ARGS) < 2:
+        GREPY_PARSER.print_help()
+        sys.exit(2)
+    # pick out PATTERN, FILE(s) and directories, while ignoring (symbolic) links
+    PATTERN  = ARGS[0]
+    NOLINKS  = [arg for arg in ARGS[1:] if not os.path.islink(arg)]
+    ARGFILES = [arg for arg in NOLINKS  if os.path.isfile(arg)]
+    ARGDIRS  = [arg for arg in NOLINKS  if os.path.isdir(arg)]
+    # generate a list of all files to search for PATTERN, while ignoring links
+    FILEPATHS = ARGFILES # start with explicitely stated files
+    if OPTIONS.traverse_recursively:
+        for folder in ARGDIRS:
+            for path, _, files in os.walk(folder, topdown=False):
+                # extend file list with all non-link files
+                FILEPATHS.extend([os.path.join(path, fl) for fl in files \
+                                  if not os.path.islink(fl)])
+    #print FILEPATHS
+    # find occurences of PATTERN in all files in the list & print them
+    RE_PATTERN = re.compile(PATTERN)
+    for flpth in FILEPATHS:
+        with open(flpth, 'r') as fl:
+            for lnum, line in enumerate(fl, start=1):
+                MATCHES = RE_PATTERN.findall(line)
+                if not MATCHES:
+                    continue
+                #print MATCHES
+                # prepare output format according to options
+                OUT_STR = ""
+                if OPTIONS.print_filenames:
+                    pass
+                if OPTIONS.print_line_number:
+                    pass
